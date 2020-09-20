4fee3eff06f1cd4e1533e8e83a32d25a9fa32560
jfcameron <jf.cameron@gmail.com>
2020-09-20 18:15:49 -0400

    wip sib

--- modules/add_dependencies/add_dependencies.cmake
+++ modules/add_dependencies/add_dependencies.cmake
@@ -23,6 +23,7 @@ function(jfc_add_dependencies)
     set(TAG "dependency")
     
     function(_add_dependency aName)
+        jfc_log(STATUS TAG "name: ${aName}")
         if (NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${aName}.cmake)
             jfc_log(FATAL_ERROR ${TAG} "${CMAKE_CURRENT_SOURCE_DIR}/${aName}.cmake does not exist. This is required to instruct the loader how to build dependency \"${aName}\".")
         endif()

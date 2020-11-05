bd9ac42561e4abb5b191720f80d342ab628fb751
Joseph Cameron <jf.cameron@gmail.com>
2020-11-04 23:04:02 -0500

    added remote url to generated build info header

--- modules/generate_buildinfo/generate_buildinfo.cmake
+++ modules/generate_buildinfo/generate_buildinfo.cmake
@@ -1,8 +1,7 @@
 set(JFC_BUILDINFO_TEMPLATE_ABSOLUTE_PATH ${CMAKE_CURRENT_LIST_DIR}/buildinfo.h.in)
 
-function(jfc_generate_cmake_header) #CLEAN THIS UP
+function(jfc_generate_cmake_header)
     string(RANDOM LENGTH 15 JFC_RANDOM_128BITS)
-#    string(TOUPPER ${JFC_TARGET_PLATFORM} JFC_TARGET_PLATFORM)
 
     execute_process(COMMAND git rev-parse HEAD 
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} 
@@ -11,6 +10,10 @@ function(jfc_generate_cmake_header) #CLEAN THIS UP
     execute_process(COMMAND git log -1 --format=%cd --date=local HEAD 
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} 
         OUTPUT_VARIABLE JFC_GIT_COMMIT_DATE OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+    execute_process(COMMAND git remote get-url origin
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} 
+        OUTPUT_VARIABLE JFC_GIT_REMOTE_URL OUTPUT_STRIP_TRAILING_WHITESPACE)
 
     configure_file(${JFC_BUILDINFO_TEMPLATE_ABSOLUTE_PATH}
         ${PROJECT_BINARY_DIR}/generated_include/${PROJECT_NAME}/buildinfo.h @ONLY)
